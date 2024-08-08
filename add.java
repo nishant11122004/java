f00ecc1c858e0658e479d016b06669693a593f3e
nishant11122004 <nishant.17232@sakec.ac.in>
2024-08-08 11:37:46 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,13 @@
+public class AdditionExample {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Perform addition
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
